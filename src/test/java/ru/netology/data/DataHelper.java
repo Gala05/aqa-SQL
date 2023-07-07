a1b87df06b904f5198db753d64be0000f40fcea9
Galina Mikhaylova <gala6858@mail.ru>
2023-07-07 16:26:29 +0300

    add positive test

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,36 @@
+package ru.netology.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+    public static AuthInfo getAuthInfoWithTestData() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AuthCode {
+        private String id;
+        private String user_id;
+        private String code;
+        private String created;
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+}
---
+++ src/test/java/ru/netology/test/BankLoginTest.java
@@ -0,0 +1,25 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class BankLoginTest {
+    @AfterAll
+    static void teardown() {
+        SQLHelper.cleanDataBase();
+    }
+
+    @Test
+    void shouldSuccessfulLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+}
